143d48ff39c2d24c5d18d3db1d69cc30137adfe4
Chilambarasan M <chilambarasan2004@gamil.com>
2026-07-25 03:41:15 +0530

    Add shared api.js with deployed backend URL

---
+++ frontend/src/api.js
@@ -0,0 +1,10 @@
+﻿import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://yarnit-visibility-3.onrender.com/api',
+  headers: {
+    'X-API-Key': process.env.REACT_APP_API_KEY || '',
+  },
+});
+
+export default api;
